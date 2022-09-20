15912c55a6c3bab003a351d5248319e7bb88e1d3
Tommimon <tommimon@gmail.com>
2022-09-20 14:51:22 +0200

    Day 1

---
+++ 2015/d01/part1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+
+int main() {
+    std::ifstream ifs("input");
+    std::string input((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+    std::cout << input << std::endl;
+    int floor = 0;
+    int firstBasement = 0;
+    for (int i = 0; i < input.size(); ++i) {
+        floor += -1 + 2 * (input[i] == '(');
+        if (floor == -1 && firstBasement == 0)
+            firstBasement = i+1;
+    }
+    std::cout << floor << std::endl;
+    std::cout << firstBasement << std::endl;
+}
